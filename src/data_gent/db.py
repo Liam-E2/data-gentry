646d325a034d4355c32c39b8611f2cb51d737972
Liam <learley1@babson.edu>
2025-12-13 19:03:36 -0500

    Add created timestamp

--- src/data_gent/db.py
+++ src/data_gent/db.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Sequence, Text
+from datetime import datetime
+
+from sqlalchemy import Sequence, Text, TIMESTAMP, func
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -13,4 +15,4 @@ class Documents(BaseTable):
 
     id: Mapped[int] = mapped_column("id", Sequence("document_id_sequence"), primary_key=True)
     content: Mapped[str] = mapped_column("content", Text())
-
+    created_at: Mapped[datetime] = mapped_column("created_at", TIMESTAMP(), server_default=func.current_timestamp())
--- test/test_db.py
+++ test/test_db.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm.session import Session
 from pytest import fixture
@@ -23,3 +25,12 @@ def test_create_documents_table(db_session):
     first = db_session.query(Documents).one()
     assert first.id == 1
     assert first.content == "test"
+    assert isinstance(first.created_at, datetime)
+
+    db_session.add(Documents(content="test2"))
+    db_session.commit()
+
+    second = db_session.query(Documents).filter(Documents.id == 2).one()
+    assert second.id == 2
+    assert second.content == "test2"
+    assert second.created_at > first.created_at
